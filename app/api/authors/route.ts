7db17bfc0613b884290df07cb797e342172eb0c0
Laurence Fass <laurencefass@hotmail.com>
2024-01-31 18:09:16 +0100

    limited save length for testing

--- app/api/authors/route.ts
+++ app/api/authors/route.ts
@@ -33,6 +33,9 @@ export async function POST(req: Request) {
   try {
     const filename = path.basename(__dirname) + ".json";
     const { data } = await req.json();
+    if (data.length > 10) {
+      return NextResponse.json({ error: 'Test only. Maximum 10 items.' });
+    }
     writeToFile(filename, data);
     initialState = data;
     return NextResponse.json({ message: 'Authors updated successfully' });

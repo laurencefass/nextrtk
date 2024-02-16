05349382a21d1711a6d03ad55a2d203a91be021f
Laurence Fass <laurencefass@hotmail.com>
2024-02-16 16:58:13 +0100

    removed the progress spinner

--- app/nprogress.tsx
+++ app/nprogress.tsx
@@ -4,6 +4,6 @@ import { Next13ProgressBar } from 'next13-progressbar';
 
 export default function NProgress() {
     return <>
-        <Next13ProgressBar height="6px" color="#704cb6" options={{ showSpinner: true }} showOnShallow />
+        <Next13ProgressBar height="6px" color="#704cb6" options={{ showSpinner: false }} showOnShallow />
     </>
 }

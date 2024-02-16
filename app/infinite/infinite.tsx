f274fe8446654fd78b8d0e4773ebb92ca30b05a3
Laurence Fass <laurencefass@hotmail.com>
2024-02-16 11:25:51 +0100

    infinite scroller ui mods

--- app/infinite/infinite.tsx
+++ app/infinite/infinite.tsx
@@ -23,12 +23,12 @@ const fetchRandomWords = (): Promise<Article> => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve({
-        title: lorem(2,5),
+        title: lorem(3, 5),
         sidebar: {
-          title: lorem (5,10),
-          body: lorem (50,75),
+          title: lorem(2, 4),
+          body: lorem(50, 75),
         },
-        header: lorem(7,12),
+        header: lorem(7, 12),
         body: paragraph(20)
       })
     }, 1000); // Delay of 1000ms (1 second)
@@ -50,7 +50,7 @@ export const InfiniteScroll: React.FC = () => {
           setLoading(false);
         });
       }
-    }, {threshold: 1.0});
+    }, { threshold: 1.0 });
 
     if (loaderRef.current) {
       observer.observe(loaderRef.current);
@@ -66,18 +66,18 @@ export const InfiniteScroll: React.FC = () => {
   return (
     <div>
       {texts.map((article, index) => (
-        <article key={index} style={{border: '1px solid #ccc', padding: '20px', margin: '10px 0', borderRadius: '5px'}}>
-        <div className="grid-container">
-          <div className="sidebar">
-            <h1>{article.sidebar.title}</h1>
-            <div>{article.sidebar.body}</div>
+        <article key={index} style={{ border: '1px solid #ccc', padding: '20px', margin: '10px 0', borderRadius: '5px' }}>
+          <div className="grid-container">
+            <div className="sidebar">
+              <h1>{article.sidebar.title}</h1>
+              <div>{article.sidebar.body}</div>
+            </div>
+            <div className="content">
+              <div className="title"><h1>{article.title}</h1></div>
+              <div className="header"><h2>{article.header}</h2></div>
+              <div className="body">{article.body}</div>
+            </div>
           </div>
-          <div className="content">
-            <div className="title"><h1>{article.title}</h1></div>
-            <div className="header"><h2>{article.header}</h2></div>
-            <div className="body">{article.body}</div>
-          </div>
-        </div>
         </article>
       ))}
       <div ref={loaderRef} style={{ height: '20px', margin: '10px 0' }}>
--- app/infinite/page.tsx
+++ app/infinite/page.tsx
@@ -3,6 +3,9 @@ import { InfiniteScroll } from './infinite'
 export default function Page() {
   return (
     <>
+      <h1>Simple Infinite Scroll</h1>
+      <p>Included to test out the IntersectionObserver API. Dummy content generated on the fly</p>
+      <p>Loading data into a simple css grid layout to present as 'articles'.</p>
       <InfiniteScroll />
     </>
   );
